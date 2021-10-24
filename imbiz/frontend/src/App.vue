cc8253ef1e8862079cc8e98c5aac5dfa641364d9
hjyoun0731 <hjyoun0731@gmail.com>
2021-10-25 08:27:01 +0900

    first commit

---
+++ imbiz/frontend/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <section class="todoapp" v-cloak>
+      <header class="header">
+        <h1>todos</h1>
+        <input class="new-todo" autofocus autocomplete="off" placeholder="What needs to be done?">
+      </header>
+      <section class="main">
+        <ul class="todo-list">
+          <li class="todo">
+            <div class="view">
+              <label>A todo item</label>
+            </div>
+          </li>
+        </ul>
+      </section>
+    </section>
+  </div>
+</template>
+
+<script>
+import "./assets/css/base.css"
+import "./assets/css/app.css"
+export default {
+  name: "app",
+  data() {
+    return {
+      todos: [],
+    }
+  }
+};
+</script>
