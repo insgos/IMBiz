e40871221f0306431be2f554eb81beb7dde4b4ec
hjyoun0731 <55075487+hjyoun0731@users.noreply.github.com>
2021-11-16 14:26:14 +0900

    Yhj (#3)
    * first commit
    
    * wails 사용 안함
    
    * layout 적용
    
    * add hello world
    
    * add docker & Makefile
    
    * add CRUD userinfo

---
+++ api/auth/auth.go
@@ -0,0 +1,29 @@
+package auth
+
+import (
+	"github.com/labstack/echo/v4"
+)
+
+func CreateUser(c echo.Context) error {
+	return nil
+}
+
+func ReadUser(c echo.Context) error {
+	return nil
+}
+
+func UpdateUser(c echo.Context) error {
+	return nil
+}
+
+func DeleteUser(c echo.Context) error {
+	return nil
+}
+
+func Login(c echo.Context) error {
+	return nil
+}
+
+func Logout(c echo.Context) error {
+	return nil
+}
--- cmd/imbizapi/imbizapi.go
+++ cmd/imbizapi/imbizapi.go
@@ -1,14 +1,25 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
+	"imbiz/api/auth"
 	"imbiz/api/router"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
 
 	e.GET("/", router.Hello)
+
+	// USER INFO CRUD
+	e.POST("/userinfo/create", auth.CreateUser)
+	e.POST("/userinfo/read", auth.ReadUser)
+	e.POST("/userinfo/update", auth.UpdateUser)
+	e.POST("/userinfo/delete", auth.DeleteUser)
+
+	e.POST("/userinfo/login", auth.Login)
+	e.POST("/userinfo/logout", auth.Logout)
 
 	e.Logger.Fatal(e.Start(":18181"))
 }
